2d342817fbe4c53a8cfeab23575111029c31135d
beppvis <visruthvijind@gmail.com>
2024-10-02 10:38:47 +0530

    starting

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-
+import math
 class Pos:
     def __init__(self,x,y):
         self.x = x
         self.y = y
 
 class Node:
-    def __init__(self,pos:Pos,g:int,h:float,parent:Node):
+    def __init__(self,pos:Pos,g:int,h:float,parent:Node=self):
         self.parent = parent
         self.f = g+h
         self.g = g
@@ -15,12 +15,22 @@ class Node:
 
 class Map:
     def __init__(self,Open:[Node],Closed:[Node]):
-        self.open = Open
-        self.closed = Closed
+        self.open_nodes = Open
+        self.closed_nodes = Closed
 
 
-
-
+i = 0
+open_nodes = []
+x = 0 
+y = 0
+while (i<10):
+    pos = Pos(x,y)
+    node = Node(pos,0,0,None)
+    open_nodes.append(node)
+    x += 1
+    y += 1
+    
+    i += 1
 
 
 
