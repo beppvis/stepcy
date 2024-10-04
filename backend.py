ed2e86da3187ca318aad23f5b14fc1c9f8c7d09c
MR7 <muthurupesh2007@gmail.com>
2024-10-04 17:10:34 +0530

    initial backend

--- backend.py
+++ backend.py
@@ -1,7 +1,8 @@
+import sqlite3
 import networkx as nx
 import matplotlib.pyplot as ply
 G = nx.Graph()
-edge=[(1,2),(2,3),(3,4),(4,5),(5,6),(6,7),(2,8),(6,9),(3,10),(4,11),(1,7)]
+edge=[(1,2),(2,3)(3,4),(4,5),(5,6),(6,7),(2,8),(6,9),(3,10),(4,11),(1,7)]
 G.add_edges_from(edge)
 distances={(1,2):10,(2,3):3,(3,4):9,(4,5):6,(5,6):7,(6,7):4,(2,8):8,(6,9):5,(3,10):11,(4,11):9,(1,7):2}
 nx.set_edge_attributes(G,distances,'weight')
